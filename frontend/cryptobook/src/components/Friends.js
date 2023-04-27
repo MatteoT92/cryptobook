8aaa7d03b0e678c26fab8608fc78fe3a012f9d49
Matteo Tartaglione <61627873+MatteoT92@users.noreply.github.com>
2023-04-27 17:51:16 +0200

    Aggiunta logica della search bar

--- frontend/cryptobook/src/components/Friends.js
+++ frontend/cryptobook/src/components/Friends.js
@@ -6,25 +6,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Friends(props) {
 
     const [friends, setFriends] = useState(props.data);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-        setFriends(props.data);
-    }, [props.data]);
+        if (document.getElementById("search").value === "") {
+            setFriends(props.data);
+        } else {
+            setFriends(friends);
+        }
+    }, [props.data, friends]);
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const filteredFriends = (e) => {
+        let searchedFriends = friends.filter(friend => friend.username.toLowerCase().includes(search.toLowerCase()));
+        setFriends(searchedFriends);
+    }
 
     return (
         <div>
             <div>
-                <form class="d-flex m-1" role="search">
-                    <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-light" type="submit">
-                        <i class="bi bi-search"></i>
+                <div className="d-flex m-1" role="search">
+                    <input className="form-control" type="search" id="search" placeholder="Search" value={search} onChange={handleSearch} />
+                    <button className="btn btn-light" type="button" onClick={filteredFriends}>
+                        <i className="bi bi-search"></i>
                     </button>
-                </form>
+                </div>
             </div>
             <div className="box-friends">
                 {friends.map((friend, i) => (
-                    <div className="friend" onClick={() => sessionStorage.setItem("friend", friend.username)}>
-                        <User name={friend.username} key={i} />
+                    <div className="friend" key={i} onClick={() => sessionStorage.setItem("friend", friend.username)}>
+                        <User name={friend.username} />
                     </div>
                 ))}
             </div>
--- frontend/cryptobook/src/components/Messages.js
+++ frontend/cryptobook/src/components/Messages.js
@@ -57,7 +57,7 @@ function Messages(props) {
             <div>
                 <div className="d-inline-flex">
                     <User name={sessionStorage.getItem("friend")} />
-                    <i class="bi bi-arrow-left-right h1"></i>
+                    <i className="bi bi-arrow-left-right h1"></i>
                     <User name={sessionStorage.getItem("user")} />
                 </div>
             </div>
